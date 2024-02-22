b032e2059aff68bc8cc1d2775be925b2586d7967
Tigelbtw <160777074+Tigelbtw@users.noreply.github.com>
2024-02-22 10:03:00 -0500

    Add files via upload

---
+++ src/main/java/com/playerattacksounds/PlayerAttackSoundsPlugin.java
@@ -0,0 +1,107 @@
+package com.playerattacksounds;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.events.StatChanged;
+import net.runelite.api.Skill;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.api.events.FocusChanged;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Player Attack Sounds"
+)
+public class PlayerAttackSoundsPlugin extends Plugin
+{
+	private long previousSoundTime = 0;
+	private boolean newAttackPossible = true;
+	private boolean isFocused = true;
+	@Inject
+	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
+
+	@Inject
+	private PlayerAttackSoundsConfig config;
+
+	@Override
+	protected void startUp()
+	{
+		if (client.getGameState() == GameState.LOGGED_IN)
+		{
+			clientThread.invokeLater(() ->
+			{
+				previousSoundTime = System.currentTimeMillis()+1000;
+			});
+		}
+	}
+
+	@Subscribe
+	protected void onConfigChanged(ConfigChanged configChanged)
+	{
+		if ("AttackSound".equals(configChanged.getGroup()))
+		{
+			if ("soundID".equals(configChanged.getKey()))
+			{
+				clientThread.invoke(() -> client.playSoundEffect(config.soundID(), config.volume()));
+			}
+		}
+	}
+
+	/*@Subscribe
+	protected void onStatChanged(StatChanged event)
+	{
+		if (event.getSkill() == Skill.HITPOINTS)
+		{
+			playSound();
+		}
+	}*/
+
+	@Subscribe
+	public void onFocusChanged(FocusChanged event)
+	{
+		isFocused = event.isFocused();
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick tick)
+	{
+		if (newAttackPossible && AnimationData.fromId(client.getLocalPlayer().getAnimation()) != null)
+		{
+				playSound();
+				newAttackPossible = false;
+		}
+
+		if (!newAttackPossible && AnimationData.fromId(client.getLocalPlayer().getAnimation()) == null)
+		{
+			newAttackPossible = true;
+		}
+	}
+
+	public void playSound()
+	{
+		if (config.soundsOnlyWhileFocused() && !isFocused) return;
+		if (System.currentTimeMillis() - previousSoundTime > config.muteDuration())
+		{
+			client.playSoundEffect(config.soundID(), config.volume());
+			previousSoundTime = System.currentTimeMillis();
+		}
+	}
+
+	@Provides
+	PlayerAttackSoundsConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(PlayerAttackSoundsConfig.class);
+	}
+}
